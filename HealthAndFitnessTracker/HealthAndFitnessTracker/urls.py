31352e2ce8fb214cd2ea4bd6d0b6597559f5dd34
Rachel Pavlakovic <rachelpav@comcast.net>
2018-03-27 21:55:48 -0400

    login and logout working without chaning the venv at all
    Signed-off-by: Rachel Pavlakovic <rachelpav@comcast.net>

--- HealthAndFitnessTracker/HealthAndFitnessTracker/urls.py
+++ HealthAndFitnessTracker/HealthAndFitnessTracker/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^admin/', admin.site.urls),

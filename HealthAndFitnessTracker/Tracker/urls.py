2c8d546d3cf6e87849ecb7b6a7f00f36f480a8fe
Rachel Pavlakovic <rachelpav@comcast.net>
2018-03-27 21:02:59 -0400

    login additions
    Signed-off-by: Rachel Pavlakovic <rachelpav@comcast.net>

--- HealthAndFitnessTracker/Tracker/urls.py
+++ HealthAndFitnessTracker/Tracker/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from . import views
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
-    path(r'^$', views.home, name="home"),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
 ]

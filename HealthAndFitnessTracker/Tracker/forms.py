6d7c6a49cb1b048dfc2234ef977c6a69fcff9c80
jth106 <34197895+jth106@users.noreply.github.com>
2018-04-18 20:23:14 -0400

    Add files via upload

---
+++ HealthAndFitnessTracker/Tracker/forms.py
@@ -0,0 +1,41 @@
+from django.forms import ModelForm
+from models import FoodInformation
+from models import DrinkInformation
+from models import ExerciseInformation
+
+class FoodForm(ModelForm):
+    class Meta:
+        model = FoodInformation
+        fields = ['name', 'density', 'caloricDensity']
+
+# Creating a form to add food info
+foodForm = FoodForm()
+
+# Creating a form to change an existing article.
+food = FoodInformation.objects.get(pk=1)
+foodForm = FoodForm(instance=food)
+
+
+class DrinkForm(ModelForm):
+    class Meta:
+        model = DrinkInformation
+        fields = ['name', 'calPerFlOz']
+
+# Creating a form to add drink information
+drinkForm = DrinkForm()
+
+# Creating a form to change an existing article.
+drink = DrinkInformation.objects.get(pk=1)
+drinkForm = DrinkForm(instance=food)
+
+class ExerciseForm(ModelForm):
+    class Meta:
+        model = ExerciseInformation
+        fields = ['name', 'calPerHour']
+
+# Creating a form to add drink information
+exerciseForm = ExerciseForm()
+
+# Creating a form to change an existing article.
+exercise = ExerciseInformation.objects.get(pk=1)
+exerciseForm = ExerciseForm(instance=food)

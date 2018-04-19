a573b322350d9d3eb92e52c8b151d544bfe2d5a2
unknown <twl23@case.edu>
2018-04-18 22:00:38 -0400

    Added new user adding functionality to site - should improve creation of user by addding in default settings and creating relevant logs in database

--- HealthAndFitnessTracker/Tracker/views.py
+++ HealthAndFitnessTracker/Tracker/views.py
@@ -1,4 +1,11 @@
 from django.views.generic.base import TemplateView
+from .forms import *
+from django.views.generic.edit import FormView
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+
 
 class HomePageView(TemplateView):
 
@@ -19,3 +26,21 @@ class exerciseTracker(TemplateView):
 class settingsAndProfile(TemplateView):
 
     template_name = "settingsAndProfile.html"
+
+class createUser(FormView):
+
+    template_name = "createUser.html"
+    form_class = UserInformationForm
+    success_url = '/home'
+
+    def post(self, request):
+        if request.method == "POST":
+            form = UserInformationForm(request.POST)
+            if form.is_valid():
+                new_user = User.objects.create_user(**form.cleaned_data)
+                # redirect, or however you want to get to the main view
+                return HttpResponseRedirect('/home/')
+        else:
+            form = UserInformationForm()
+
+        return render(request, 'createUser.html', {'form': form})

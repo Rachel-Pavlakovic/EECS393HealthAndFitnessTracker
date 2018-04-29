8249ab15762a546915047d0e5c87d8b603608e69
unknown <twl23@case.edu>
2018-04-29 17:56:14 -0400

    Alerts added, one hour implemented and late alerts implemented

--- HealthAndFitnessTracker/Tracker/forms.py
+++ HealthAndFitnessTracker/Tracker/forms.py
@@ -5,25 +5,30 @@ from django.contrib.auth.models import User
 class FoodForm(ModelForm):
     class Meta:
         model = FoodLog
-        fields = ['info','quantity', 'date']
+        fields = ['info', 'quantity', 'date']
 
 class DrinkForm(ModelForm):
     class Meta:
         model = DrinkLog
-        fields = ['info','quantity', 'date']
+        fields = ['info', 'quantity', 'date']
 
 class ExerciseForm(ModelForm):
     class Meta:
         model = ExerciseLog
-        fields = ['info','duration', 'date']
+        fields = ['info', 'duration', 'date']
 
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
 
+class AlertForm(ModelForm):
+    class Meta:
+        model = AlertLog
+        fields = ['description', 'time']
+
 class UserInformationForm(ModelForm):
     class Meta:
         model = UserInformation
-        fields = ['weight', 'height', 'gender', 'units', 'notificationType', 'phoneNumber', 'email']
+        fields = ['weight', 'height', 'gender', 'units', 'notificationType', 'phoneNumber']
 
